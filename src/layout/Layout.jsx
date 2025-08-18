af79aa6a034e3c6c9638b5b0b3cb4010666545f4
niepomaker <paprotlukasz@gmail.com>
2025-08-18 19:24:28 +0200

    fix responsibility again

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,18 +4,22 @@ import "../styles/Glass.css";
 const Layout = () => {
   return (
     <div className="app-container">
-      <nav className="sidebar">
-        <h2 className="logo">Łukasz Paprot</h2>
-        <ul>
-        <NavLink to="/home" className="nav-button">Home</NavLink>
-        <NavLink to="/experience" className="nav-button">Experience</NavLink>
-        <NavLink to="/education" className="nav-button">Education</NavLink>
-        <NavLink to="/technologies" className="nav-button">Technologies</NavLink>
-        <NavLink to="/hobbies" className="nav-button">Hobbies</NavLink>
-        <NavLink to="/contact" className="nav-button">About Me</NavLink>
-        </ul>
-      </nav>
-      <Outlet />
+        <div className="main-layout">
+          <nav className="sidebar">
+            <h2 className="logo">Łukasz Paprot</h2>
+            <ul>
+                <NavLink to="/home" className="nav-button">Home</NavLink>
+                <NavLink to="/experience" className="nav-button">Experience</NavLink>
+                <NavLink to="/education" className="nav-button">Education</NavLink>
+                <NavLink to="/technologies" className="nav-button">Technologies</NavLink>
+                <NavLink to="/hobbies" className="nav-button">Hobbies</NavLink>
+                <NavLink to="/contact" className="nav-button">About Me</NavLink>
+            </ul>
+          </nav>
+          <div className="main-content">
+            <Outlet />
+        </div>
+        </div>
     </div>
   );
 };
